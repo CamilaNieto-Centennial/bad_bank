f019488159f6892a326442c6de15cdcbba08b33d
CamilaNieto-Centennial <fourcamilitanieto@gmail.com>
2023-01-16 18:47:54 -0500

    Bad Bank Routing

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 function Spa(){
     return(
-        <>
-        <h1>Welcome to Bad Bank.</h1>
-        <NavBar/>
-        </>
+        <HashRouter>
+            <div>
+                <NavBar/>
+                <hr/>
+                <Route path="/" exact component={Home} />
+                <Route path="/CreateAccount/" component={CreateAccount} />
+                <Route path="/login/" component={Login} />
+                <Route path="/deposit/" component={Deposit} />
+                <Route path="/withdraw/" component={Withdraw} />
+                <Route path="/balance/" component={Balance} />
+                <Route path="/alldata/" component={AllData} />
+            </div>
+        </HashRouter>
     )
 }
 
