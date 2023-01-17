73a2dcf6cf4d4bc4037363a2d83d64e08dd6cfa8
CamilaNieto-Centennial <fourcamilitanieto@gmail.com>
2023-01-17 18:42:03 -0500

    BankForm Component Created and Applied in Create Account Page

--- context.js
+++ context.js
@@ -30,3 +30,95 @@ function Card(props) {
         </div>
     )
 }
+
+
+function BankForm(props, {chooseShowP}) {
+
+    const [show, setShow] = React.useState(true);
+    const [status, setStatus] = React.useState('');
+    const [name, setName] = React.useState('');
+    const [email, setEmail] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const ctx = React.useContext(UserContext)
+
+    function validate(field, label) {
+        if (!field) {
+            console.log('Error: ' + label)
+            setStatus('Error: ' + label);
+            setTimeout(() => setStatus(''), 3000);
+            props.chooseStatusP('Error: ' + label);
+            setTimeout(() => props.chooseStatusP(''), 3000);
+            return false;
+        }
+        return true;
+    }
+
+    function handleCreate() {
+        console.log(name, email, password);
+        if(!validate(name, 'name')) return;
+        if(!validate(email, 'email')) return;
+        if(!validate(password, 'password')) return;
+        ctx.users.push({name, email, password, balance:100})
+        setShow(false)
+        props.chooseShowP(false)
+    }
+
+    function clearForm() {
+        setName('');
+        setEmail('');
+        setPassword('');
+        setShow(true)
+        props.chooseShowP(true)
+    }
+
+    return (
+            <>
+            {props.statusP && (<h5 id="createStatus" className="text-danger"><strong>{props.statusP}</strong></h5>)}
+            {props.name && (
+                <>
+                    <label htmlFor={props.name}>Name</label>
+                    <input type="input" className="form-control" id={props.name} placeholder="Enter your name" value={name} onChange={e => setName(e.currentTarget.value)}/>
+                    <br />
+                </>
+                )
+            }
+            {props.email && (
+                <>
+                    <label htmlFor={props.email}>Email</label>
+                    <input type="email" className="form-control" id={props.email} placeholder="email@email.com" value={email} onChange={e => setEmail(e.currentTarget.value)}/>
+                    <br />
+                </>
+                )
+            }
+            {props.password && (
+                <>
+                    <label htmlFor={props.password}>Password</label>
+                    <input type="password" className="form-control" id={props.password} placeholder="" value={password} onChange={e => setPassword(e.currentTarget.value)}/>
+                    <br />
+                </>
+                )
+            }
+            {props.message && (
+                <>
+                    <h5 id={props.message}><strong>Success</strong></h5>
+                    <br />
+                </>
+                )
+            }
+            {props.buttonCreate && (
+                <>
+                    <button type="submit" className="btn btn-secondary" onClick={handleCreate}>{props.buttonCreate}</button>
+                </>
+                )
+            }
+            {props.buttonAdd && (
+                <>
+                    <button type="submit" className="btn btn-secondary" onClick={clearForm}>{props.buttonAdd}</button>
+                </>
+                )
+            }
+            </>
+    )
+
+
+}
--- createaccount.js
+++ createaccount.js
@@ -1,35 +1,13 @@
-function CreateAccount(){
-    const [show, setShow] = React.useState(true);
-    const [status, setStatus] = React.useState('');
-    const [name, setName] = React.useState('');
-    const [email, setEmail] = React.useState('');
-    const [password, setPassword] = React.useState('');
-    const ctx = React.useContext(UserContext)
+function CreateAccount(props){
+    const [showP, setShowP] = React.useState(true);
+    const [statusP, setStatusP] = React.useState('');
 
-    function validate(field, label) {
-        if (!field) {
-            setStatus('Error: ' + label);
-            setTimeout(() => setStatus(''), 3000);
-            return false;
-        }
-        return true;
-    }
-
-    function handleCreate() {
-        console.log(name, email, password);
-        if(!validate(name, 'name')) return;
-        if(!validate(email, 'email')) return;
-        if(!validate(password, 'password')) return;
-        ctx.users.push({name, email, password, balance:100})
-        setShow(false)
-    }
-
-    function clearForm() {
-        setName('');
-        setEmail('');
-        setPassword('');
-        setShow(true)
-    }
+    const chooseShowP = (showP) => {
+        setShowP(showP);
+    };
+    const chooseStatusP = (statusP) => {
+        setStatusP(statusP);
+    };
 
     return(
         <div className="container" style={{margin: "2em"}}>
@@ -39,34 +17,28 @@ function CreateAccount(){
                 header="Create Account"
                 headercolor = "#ffffff"
                 headerBackground = "#1b2a41"
-                status={status}
-                body={show 
+                body={showP
                     ? (
-                        <>
-                        <label htmlFor="name">Name</label>
-                        <input type="input" className="form-control" id="name" placeholder="Enter your name" value={name} onChange={e => setName(e.currentTarget.value)}/>
-                        <br />
-                        <label htmlFor="email">Email</label>
-                        <input type="email" className="form-control" id="email" placeholder="email@email.com" value={email} onChange={e => setEmail(e.currentTarget.value)}/>
-                        <br />
-                        <label htmlFor="password">Password</label>
-                        <input type="password" className="form-control" id="password" placeholder="" value={password} onChange={e => setPassword(e.currentTarget.value)}/>
-                        <br />
-                        <button type="submit" className="btn btn-secondary" onClick={handleCreate}>Create Account</button>
-                        </>
+                        <BankForm
+                            chooseStatusP={chooseStatusP}
+                            statusP={statusP}
+                            name="name"
+                            email="email"
+                            password="password"
+                            buttonCreate="Create Account"
+                            chooseShowP={chooseShowP}
+                        />
                     )
                     :(
-                        <>
-                        <h5><strong>Success</strong></h5>
-                        <button type="submit" className="btn btn-secondary" onClick={clearForm}>Add another account</button>
-                        </>
+                        <BankForm
+                            message="Success"
+                            buttonAdd="Add another account"
+                            chooseShowP={chooseShowP}
+                        />
                     )
                 }
-            />
+            >
+            </Card>
         </div>
-        /*<div>
-            <h1>Create Account</h1>
-            <h5>{JSON.stringify(ctx)}</h5>
-        </div>*/
     )
 }
