ddc0e268e30bd6d92a99cafac52c516d375a064a
CamilaNieto-Centennial <fourcamilitanieto@gmail.com>
2023-02-05 20:49:02 -0500

    Balance Page Deleted

--- balance.js
+++
@@ -1,9 +0,0 @@
-function Balance(){
-    const ctx = React.useContext(UserContext)
-    return(
-        <div>
-            <h1>Balance</h1>
-            <h5>{JSON.stringify(ctx)}</h5>
-        </div>
-    )
-}
